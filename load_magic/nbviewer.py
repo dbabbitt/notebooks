592371b6651ab69444faf2370903124e6d57e57b
Dave Babbitt <dave.babbitt@gmail.com>
2017-12-10 10:16:54 -0600

    Added nbviewer link

---
+++ load_magic/nbviewer.py
@@ -0,0 +1,5 @@
+
+from IPython.display import HTML
+
+notebook_viewer_url = 'https://nbviewer.jupyter.org/github/dbabbitt/notebooks/blob/master/' + notebook_path
+HTML('Click <a href="' + notebook_viewer_url + '" target="_blank">here</a> to view notebook in nbviewer.')
