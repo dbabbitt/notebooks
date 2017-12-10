ed2b5ba853664102f3a82ef7e4744c583599bbb9
Dave Babbitt <dave.babbitt@gmail.com>
2017-12-10 10:13:27 -0600

    Added javascript load magic

---
+++ load_magic/javascript.py
@@ -0,0 +1,8 @@
+
+%%javascript
+var kernel = IPython.notebook.kernel;
+var body = document.body,  
+    attribs = body.attributes;
+var command = "notebook_name = " + "'"+attribs['data-notebook-name'].value+"'";
+var command = "notebook_path = " + "'"+attribs['data-notebook-path'].value+"'";
+kernel.execute(command);
