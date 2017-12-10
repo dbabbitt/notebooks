29d98e2ee8034ecadc1d574372b2008c1d8a48d1
Dave Babbitt <dave.babbitt@gmail.com>
2017-12-10 07:44:22 -0600

    Added Load Magic

---
+++ load_magic/storage.py
@@ -0,0 +1,58 @@
+
+!mkdir ../data
+!mkdir ../data/csv
+!mkdir ../saves
+!mkdir ../saves/pickle
+!mkdir ../saves/csv
+import pickle
+import pandas as pd
+import os
+
+# Handy list of the different types of encodings
+encoding = ['latin1', 'iso8859-1', 'utf-8'][2]
+
+# Change this to your data and saves folders
+data_folder = r'../data/'
+saves_folder = r'../saves/'
+
+def load_csv(csv_name=None):
+    csv_folder = data_folder + 'csv/'
+    if csv_name is None:
+        csv_path = max([os.path.join(csv_folder, f) for f in os.listdir(csv_folder)],
+                       key=os.path.getmtime)
+    else:
+        csv_path = csv_folder + csv_name + '.csv'
+    data_frame = pd.read_csv(csv_path, encoding=encoding)
+    
+    return(data_frame)
+
+def load_object(obj_name):
+    pickle_path = saves_folder + 'pickle/' + obj_name + '.pickle'
+    try:
+        object = pd.read_pickle(pickle_path)
+    except:
+        with open(pickle_path, 'rb') as handle:
+            object = pickle.load(handle)
+    
+    return(object)
+
+def save_dataframes(include_index=False, **kwargs):
+    csv_folder = saves_folder + 'csv/'
+    for frame_name in kwargs:
+        if isinstance(kwargs[frame_name], pd.DataFrame):
+            csv_path = csv_folder + frame_name + '.csv'
+            kwargs[frame_name].to_csv(csv_path, sep=',', encoding=encoding,
+                                      index=include_index)
+
+# Classes, functions, and methods cannot be pickled
+def store_objects(**kwargs):
+    for obj_name in kwargs:
+        if hasattr(kwargs[obj_name], '__call__'):
+            raise RuntimeError('Functions cannot be pickled.')
+        obj_path = saves_folder + 'pickle/' + str(obj_name)
+        pickle_path = obj_path + '.pickle'
+        if isinstance(kwargs[obj_name], pd.DataFrame):
+            kwargs[obj_name].to_pickle(pickle_path)
+        else:
+            with open(pickle_path, 'wb') as handle:
+                pickle.dump(kwargs[obj_name], handle, pickle.HIGHEST_PROTOCOL)
