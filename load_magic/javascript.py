b2b8d17f1843326f9544fc2abbca55c4ea8c7b2a
Dave Babbitt <dave.babbitt@gmail.com>
2017-12-12 10:33:50 -0600

    Got rid of unused line

--- load_magic/javascript.py
+++ load_magic/javascript.py
@@ -3,6 +3,5 @@
 var kernel = IPython.notebook.kernel;
 var body = document.body,  
     attribs = body.attributes;
-var command = "notebook_name = " + "'"+attribs['data-notebook-name'].value+"'";
 var command = "notebook_path = " + "'"+attribs['data-notebook-path'].value+"'";
 kernel.execute(command);
