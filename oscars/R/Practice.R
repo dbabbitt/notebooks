fb25b6b4606424799f7bf1f08c8d0d4f1ca55e0e
Dave Babbitt <dave.babbitt@gmail.com>
2017-12-25 10:46:02 -0600

    Added practice file

--- oscars/R/Practice.R
+++ oscars/R/Practice.R
@@ -5,5 +5,6 @@ tweets.csv <- read.csv('../data/csv/tweets.csv', na.strings = c('.', 'NA', '', '
 colnames(tweets.csv) <- c('tweetID', 'tweetTime', 'tweetAuthor', 'tweetText')
 
 if (!require('chron')) install.packages('chron', repos = 'http://cran.us.r-project.org'); require('chron')
-dtparts <- strsplit(tweets.csv$tweetTime[1],' ')
-dtparts
+dtparts <- t(as.data.frame(strsplit(tweets.csv$tweetTime,' ')))
+row.names(dtparts) <- NULL
+chron(dates = dtparts[, 1], times = dtparts[, 2], format = c('y-m-d', 'h:m:s'))
