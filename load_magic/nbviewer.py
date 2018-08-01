d4ee644a6eeb1083459f78204d8f08034125882f
Dave Babbitt <dave.babbitt@gmail.com>
2018-08-01 06:44:42 -0400

    Fixedd nbviewer, local-url image display

--- load_magic/nbviewer.py
+++ load_magic/nbviewer.py
@@ -1,5 +1,6 @@
 
 from IPython.display import HTML
 
-notebook_viewer_url = 'https://nbviewer.jupyter.org/github/dbabbitt/notebooks/blob/master/' + notebook_path
+notebook_viewer_url = 'https://nbviewer.jupyter.org/github/dbabbitt/notebooks/blob/master/'
+notebook_viewer_url += '/'.join(notebook_path.split('/')[1:])
 HTML('Click <a href="' + notebook_viewer_url + '" target="_blank">here</a> to view notebook in nbviewer.')
