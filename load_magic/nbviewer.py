cff9b624e45e55c17742d9b31225e85addc936e3
Dave Babbitt <dave.babbitt@gmail.com>
2019-04-24 20:08:50 -0400

    added aoe2 feature selection notebook

--- load_magic/nbviewer.py
+++ load_magic/nbviewer.py
@@ -3,4 +3,5 @@ from IPython.display import HTML
 
 notebook_viewer_url = 'https://nbviewer.jupyter.org/github/dbabbitt/notebooks/blob/master/'
 notebook_viewer_url += '/'.join(notebook_path.split('/')[1:])
-HTML('Click <a href="' + notebook_viewer_url + '" target="_blank">here</a> to view notebook in nbviewer.')
+message_str = 'Click <a href="{}" target="_blank">here</a> to view notebook in nbviewer.'
+HTML(message_str.format(notebook_viewer_url))
