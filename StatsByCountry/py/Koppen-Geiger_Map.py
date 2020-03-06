d41258ef5598e61651e6138a7ef2d4417340ef30
Dave Babbitt <babbitt_dave@bah.com>
2020-03-06 08:39:35 -0500

    afghanistan

---
+++ StatsByCountry/py/Koppen-Geiger_Map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import choropleth_utils
+
+c = choropleth_utils.ChoroplethUtilities()
+
+file_path = r'C:\Users\577342\Downloads\Koppen-Geiger_Map_World_present.svg'
+c.trim_d_path(file_path)
