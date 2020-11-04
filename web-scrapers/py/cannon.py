2b2b9dc8ad6b5be59e8dc9aeba70ef42dcc8b78f
Dave Babbitt <dave.babbitt@gmail.com>
2020-11-04 17:31:34 -0500

    moved web scrapers to another repo

--- web-scrapers/py/cannon.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-proxies = {'http': 'http://<<USERID>>:<<PASSWORD>>@23.19.51.245:40791/'}
-
-while True:
-	try:
-		page = requests.get('http://www.vehiclessellingsolutions.com/', proxies=proxies)
-	except Exception as e:
-		print(str(e))
