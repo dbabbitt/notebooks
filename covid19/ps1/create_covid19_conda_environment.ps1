11d40fc74928976d6b59a8317bba08cf38e05c2e
Dave Babbitt <dave.babbitt@gmail.com>
2020-11-05 06:13:08 -0500

    Moved the COVID stuff to its own repo

--- covid19/ps1/create_covid19_conda_environment.ps1
+++
@@ -1,6 +0,0 @@
-
-# Set up global variables
-$DisplayName = "COVID-19"
-$RepositoryPath = "notebooks/covid19"
-$EnvironmentName = "covid19"
-."C:\Users\dev\Documents\repositories\notebooks\ps1\create_conda_environment.ps1"
